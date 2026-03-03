0334a58881662834b7436b30031e8b7216ce4361
Mohammad Shoaib <shoaibm@digiquadsolutions.com>
2026-03-03 12:27:19 +0530

    ignore some files and added test case

--- src/main/java/com/media/gallery/MediaGalleryApplication.java
+++ src/main/java/com/media/gallery/MediaGalleryApplication.java
@@ -1,12 +1,24 @@
 package com.media.gallery;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class MediaGalleryApplication {
 
+	public static Logger log = LoggerFactory.getLogger(MediaGalleryApplication.class);
+
+	@PostConstruct
+	public void init(){
+		log.info("Application started....");
+	}
+
 	public static void main(String[] args) {
+		log.info("Application executed...");
 		SpringApplication.run(MediaGalleryApplication.class, args);
 	}
 
--- src/test/java/com/media/gallery/MediaGallaeyApplicationTests.java
+++ src/test/java/com/media/gallery/MediaGallaeyApplicationTests.java
@@ -1,13 +1,21 @@
 package com.media.gallery;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MediaGallaeyApplicationTests {
 
+	public static Logger log = LoggerFactory.getLogger(MediaGallaeyApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		log.info("Test cases executing...");
+		assertEquals(true,true);
 	}
 
 }
