fa8e3f4d0310c19de2e390de6b8e51ee7fa78bc3
Mohammad Shoaib <shoaibm@digiquadsolutions.com>
2026-03-03 14:51:47 +0530

    check with errors

--- src/test/java/com/media/gallery/MediaGallaeyApplicationTests.java
+++ src/test/java/com/media/gallery/MediaGallaeyApplicationTests.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MediaGallaeyApplicationTests {
